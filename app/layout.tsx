d4cca376b592257bd95c1647e439487dc4e92d20
Bryan Paredes <bryansparedes2@gmail.com>
2024-12-05 19:06:27 -0600

    principal page with layout and error page

---
+++ app/layout.tsx
@@ -0,0 +1,57 @@
+import "@/styles/globals.css";
+import { Metadata, Viewport } from "next";
+import { Link } from "@nextui-org/link";
+import clsx from "clsx";
+
+import { Providers } from "./providers";
+
+import { siteConfig } from "@/config/site";
+import { fontSans } from "@/config/fonts";
+import { NavbarComponent } from "@/components/navbar";
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: {
+    icon: "/LOGO-TPCEXPRESS.ico",
+    apple: "/LOGO-TPCEXPRESS.ico",
+    shortcut: "/LOGO-TPCEXPRESS.ico",
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html suppressHydrationWarning className="dark" lang="es">
+      <head />
+      <body
+        className={clsx(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+          <div className="relative flex flex-col h-screen">
+            <NavbarComponent />
+            <main className="container mx-auto max-w-5xl pt-10 px-6 flex-grow">
+              {children}
+            </main>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import HeroPage from "@/components/hero";
+import WorkPage from "@/components/work";
+
+export default function Home() {
+  return (
+    <>
+      <HeroPage />
+      <WorkPage />
+    </>
+  );
+}
