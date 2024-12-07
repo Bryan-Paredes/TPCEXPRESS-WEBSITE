2824e1d55c496ae301fc52ffa82e83e1181e089b
Bryan Paredes <bryansparedes2@gmail.com>
2024-12-07 11:49:01 -0600

    Working in work section

--- app/page.tsx
+++ app/page.tsx
@@ -9,3 +9,8 @@ export default function Home() {
     </>
   );
 }
+
+// git branch -m dev develop
+// git fetch origin
+// git branch -u origin/develop develop
+// git remote set-head origin -a
--- sections/work.tsx
+++ sections/work.tsx
@@ -3,14 +3,19 @@ import { subtitle, title } from "@/components/primitives";
 
 export default function WorkPage() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 mt-6 md:py-10">
-      <div className="inline-block max-w-lg text-center justify-center">
+    <section className="mt-6 flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-lg justify-center text-center">
         <h5 className={subtitle({ class: "uppercase text-primary" })}>
           Nuestro Servicio
         </h5>
         <h1 className={title({ class: "font-bold" })}>Como Funciona?</h1>
       </div>
-      <div className="grid grid-cols-3 gap-10 w-full itmes-center justify-center text-center mt-10">
+      <div className="itmes-center mt-10 grid w-full grid-cols-3 justify-center gap-10 text-center">
+        <div>
+          <p>Image</p>
+          <h3>Titulo</h3>
+          <p>Descripcion</p>
+        </div>
         <div>
           <p>Image</p>
           <h3>Titulo</h3>
