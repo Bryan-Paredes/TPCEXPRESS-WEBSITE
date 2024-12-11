d7603b75d1e06d2c3626e55d1f1962d0a2fe81b5
Bryan Paredes <bryansparedes2@gmail.com>
2024-12-10 20:44:55 -0600

    creating site config for reuse in specfic components

---
+++ src/config/site.ts
@@ -0,0 +1,15 @@
+export const menuItems = [
+    {
+        name: "Nosotros",
+        href: "/nosotros",
+    },
+    {
+        name: "Estándar",
+        href: "/estandar",
+    },
+    {
+        name: "COD",
+        href: "/cod",
+    },
+
+];
