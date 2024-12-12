919463baf094f0716ce6e7db62a93e8d91489a1e
Bryan Paredes <bryansparedes2@gmail.com>
2024-12-11 20:48:51 -0600

    Creating content and import Ui component to show each review

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,3 +1,60 @@
+import { cn } from "@/lib/utils";
+import Marquee from "@/components/ui/marquee";
+import { reviews } from "@/config/site";
+
+const firstRow = reviews.slice(0, reviews.length / 2);
+
+const ReviewCard = ({
+  img,
+  name,
+  username,
+  body,
+}: {
+  img: string;
+  name: string;
+  username: string;
+  body: string;
+}) => {
+  return (
+    <figure
+      className={cn(
+        "relative w-64 cursor-pointer overflow-hidden rounded-xl border p-4",
+        // light styles
+        "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
+        // dark styles
+        "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]"
+      )}
+    >
+      <div className="flex flex-row items-center gap-2">
+        <img className="rounded-full" width="32" height="32" alt="" src={img} />
+        <div className="flex flex-col">
+          <figcaption className="text-sm font-medium dark:text-white">
+            {name}
+          </figcaption>
+          <p className="text-xs font-medium dark:text-white/40">{username}</p>
+        </div>
+      </div>
+      <blockquote className="mt-2 text-sm">{body}</blockquote>
+    </figure>
+  );
+};
+
 export default function Testimonials() {
-  return <div>Testimonials</div>;
+  return (
+    <section className="container">
+      <div className="flex flex-col items-start justify-center mt-14">
+        <h1 className="font-bold text-4xl">Testimonios</h1>
+        <h5 className="uppercase text-primary">Que dicen nuestros clientes</h5>
+      </div>
+      <div className=" relative flex h-[200px] w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-background ">
+        <Marquee pauseOnHover className="[--duration:45s]">
+          {firstRow.map((review) => (
+            <ReviewCard key={review.username} {...review} />
+          ))}
+        </Marquee>
+        <div className="pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r from-white dark:from-background"></div>
+        <div className="pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l from-white dark:from-background"></div>
+      </div>
+    </section>
+  );
 }
