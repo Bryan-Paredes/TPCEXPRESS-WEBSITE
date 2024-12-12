84ac3f3f60b4436b78c367a308201b40b59ba51b
Bryan Paredes <bryansparedes2@gmail.com>
2024-12-11 18:40:25 -0600

    fix wrong gramatic in svg

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -14,9 +14,9 @@ export const WhatsappIcon: React.FC<IconSvgProps> = ({
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="icon icon-tabler icons-tabler-outline icon-tabler-brand-whatsapp"
       {...props}
     >
