35aa5ff1a92daa21a3e9d88e7e1c8ab8a9d3b09d
Bryan Paredes <bryansparedes2@gmail.com>
2024-12-12 21:06:29 -0600

    creating new button component

---
+++ src/components/Buttons.tsx
@@ -0,0 +1,50 @@
+import { Button, Link } from "@nextui-org/react";
+import { Route } from "lucide-react";
+import { SendServiceIcon, WhatsappIcon } from "./icons";
+
+function HeaderButtons() {
+  return (
+    <div className="flex gap-4 items-center my-5">
+      <Button
+        as={Link}
+        variant="ghost"
+        color="primary"
+        href="/quote"
+        className="uppercase font-semibold"
+        startContent={<Route size={20} />}
+      >
+        Cotización
+      </Button>
+      <Button
+        as={Link}
+        isExternal
+        variant="ghost"
+        color="success"
+        href="https://wa.me/5511991525259"
+        className="uppercase font-semibold"
+        startContent={<WhatsappIcon size={20} />}
+      >
+        Whatsapp
+      </Button>
+    </div>
+  );
+}
+
+function ServiceButton() {
+  return (
+    <Button
+      as={Link}
+      size="lg"
+      variant="solid"
+      color="primary"
+      radius="lg"
+      href="/quote"
+      className="text-white"
+      startContent={<SendServiceIcon size={50} />}
+    >
+      Solicitar Servicio
+    </Button>
+  );
+}
+
+export { HeaderButtons, ServiceButton };
