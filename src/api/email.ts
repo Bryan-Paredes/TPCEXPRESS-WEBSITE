c23b066b3c9060aca9fbf2c78b3669bae4999cff
Bryan Paredes <bryansparedes2@gmail.com>
2025-01-21 21:29:47 -0600

    Actualizar la URL del servidor de correo y agregar funcionalidad para enviar envíos

--- src/api/email.ts
+++ src/api/email.ts
@@ -7,7 +7,7 @@ export const sendEmail = async (data: {
   terms: boolean;
 }): Promise<{ success: boolean; message: string }> => {
   try {
-    const response = await fetch('http://localhost:3000/sendContact', {
+    const response = await fetch('https://email-server-tpcexpress.onrender.com/sendContact', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -21,6 +21,46 @@ export const sendEmail = async (data: {
 
     return await response.json();
   } catch (error) {
-    throw error;
+    throw new Error(`Error al enviar el correo: ${error}`);
   }
 };
+
+export const sendShip = async (data: {
+  origenEnvio: string;
+  nombreEnvio: string;
+  numeroRemitente: string;
+  correoRemitente: string;
+  direccionRemitente: string;
+  textRecoleccion?: string;
+  origenDestino: string;
+  nombreDestino: string;
+  numeroDestino: string;
+  correoDestino: string;
+  direccionDestino: string;
+  textDestino?: string;
+  nit?: string;
+  nombreNit?: string;
+  banco?: string;
+  tipoCuenta?: string;
+  numeroCuenta?: string;
+  nombreCuenta?: string;
+  terms: boolean;
+}): Promise<{ success: boolean; message: string }> => {
+  try {
+    const response = await fetch('https://email-server-tpcexpress.onrender.com/sendEnvio', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error al enviar el correo: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    throw new Error(`Error al enviar el correo: ${error}`);
+  }
+}
