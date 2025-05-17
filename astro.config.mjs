87c13ab8cb7034098aac16829eeabea232228ac1
Bryan Paredes <bryansparedes2@gmail.com>
2025-05-16 19:39:32 -0600

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig, envField } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   env: {
@@ -11,5 +13,11 @@ export default defineConfig({
       API_TOKEN: envField.string({ context: "client", access: "public" }),
     },
   },
+
   integrations: [react(), tailwind({ applyBaseStyles: false })],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
